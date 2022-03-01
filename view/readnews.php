e7349da1507711d444b1882fa19f5419fff9d94d
Maxim-Rider <maksitrall5@gmail.com>
2022-03-01 12:59:18 +0200

    PROJ-39-доработка-view

--- view/readnews.php
+++ view/readnews.php
@@ -9,6 +9,13 @@
 <?php
 
 ViewNews::ReadNews($n);
+
+echo "<br>";
+Controller::Comments($_GET['id']);
+
+echo "<br>";
+ViewComments::CommentsForm();
+
 $content = ob_get_clean();
 include_once 'view/layout.php';
 
