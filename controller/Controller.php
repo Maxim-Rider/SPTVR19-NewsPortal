defba292c87778359a0de492473b64936db5a707
Ekaterina <ekaterina_2001@inbox.ru>
2022-03-02 13:22:54 +0200

    ...

--- controller/Controller.php
+++ controller/Controller.php
@@ -37,6 +37,22 @@ class Controller {
         header('Location:news?id='.$id.'#ctable');
     }
 
-   
+    //список комментариев
+    public static function Comments ($newsid) {
+        $arr = Comments::getCommentByNewsID ($newsid);
+        ViewComments::CommentsByNews ($arr);
+    }
+
+    //количество комментариев к новости
+    public static function CommentsCount ($newsid) {
+        $arr = Comments::getCommentsCountByNewsID ($newsid);
+        ViewComments::CommentsCount ($arr);
+    }
+
+    //ссылка - переход к списку комментариев
+    public static function CommentsCountWithAncor ($newsid) {
+        $arr = Comments::getCommentsCountByNewsID ($newsid);
+        ViewComments::CommentsCountWithAncor ($arr);
+    }
 }//end class
 
