00a556da79d88de53ec5e6a0f8a5c33a296d9f58
Maxim-Rider <maksitrall5@gmail.com>
2022-03-24 09:17:47 +0200

    PROJ-64-доработка-routing-admin

--- admin/routeAdmin/routingAdmin.php
+++ admin/routeAdmin/routingAdmin.php
@@ -19,6 +19,11 @@ elseif ($path == 'logout')
     //Выход
     $response = controllerAdmin::logoutAction();
 }
+//----------------listnews
+elseif($path == 'newsAdmin')
+{
+    $response=controllerAdminNews::NewsList();
+}
 
 else
 {
